90f4e383d5a6c65a4050e8d4e675664e3efb542c
ArseniiT <arseniit@gmail.com>
2018-10-21 23:50:57 -0400

    add css styles with require() to main.js

--- src/components/Hi.vue
+++ src/components/Hi.vue
@@ -1,10 +1,22 @@
 <template>
+  
   <div>
+    <!-- <link href="./" rel="stylesheet" type="text/css"> -->
+
     <button @click="show = !show">
       Переключить отрисовку
     </button>
-    <transition name="slide-fade">
+    <transition 
+    name="slide-fade"
+    enter-active-class="animated bounceInLeft"
+    leave-active-class="animated bounceOutRight">
       <p v-if="show">привет</p>
+    </transition>
+    <transition 
+    name="slide"
+    enter-active-class="animated bounceInLeft"
+    leave-active-class="animated bounceOutRight">
+      <p v-if="!show">пока</p>
     </transition>
   </div>
 </template>
@@ -22,25 +34,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("../../node_modules/animate.css/animate.min.css");
-//@import "https://raw.githubusercontent.com/daneden/animate.css/master/animate.css";
-.slide-fade-enter-active {
-  //transition: all .3s ease;
-  /* animation: animated infinite bounce delay-2s; */
-  @extend .animated, .infinite, .bounce, .delay-2s !optional;
 
-  
-}
-.slide-fade-leave-active {
-  /* transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0); */
-  /* animation: animated infinite bounce delay-2s; */
-  //@extend .animated;
-/*   @extend .bounce;
-  @extend .infinite;
-  @extend .delay-2s; */
-}
-.slide-fade-enter, .slide-fade-leave-to {
-  transform: translateX(10px);
-  opacity: 0;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+require('../node_modules/animate.css/animate.min.css')
+
 Vue.config.productionTip = false
 
 new Vue({
